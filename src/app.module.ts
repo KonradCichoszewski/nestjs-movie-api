0dc49bd4d4476e9b5e537e0302e1d5e68335a312
Konrad Cichoszewski <konrad.cichoszewski@netguru.com>
2023-09-15 06:33:26 +0300

    autogenerate movie and genre modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MovieModule } from './movie/movie.module';
+import { GenreModule } from './genre/genre.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MovieModule, GenreModule],
 })
 export class AppModule {}
