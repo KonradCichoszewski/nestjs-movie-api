28116001e55092388e6b51b57b4bea79a3cce8c6
Konrad Cichoszewski <konrad.cichoszewski@netguru.com>
2023-09-18 19:50:16 +0300

    add comments to the code

--- src/shared/prisma.service.ts
+++ src/shared/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+// This service is used to inject the PrismaClient into other services and interact with the database
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
